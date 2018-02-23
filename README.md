37c728ff0e9bc67d1176fe1046aa3c3fb5288401
Darshan Patel <darshan7230@gmail.com>
2018-02-23 11:29:05 -0600

    Delete everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# cs496
-This project is for a senior software project. The intention of this project is to create a web application that allows users to create teams of celebrity based characters and use those teams to simulate battles with other players' teams.
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("php/HTML_Element.class.php");
-
-// We will build upon our index page by modular additions in the form of string concatenations
-$index = new HTML_Element("img", ["src" => "hehe.png", "style" => "width: 50%; height: 75%"]);
-echo $index;
