3c2a06c790e9fa235db61a96efb7699022b755b7
Darshan Patel <darshan7230@gmail.com>
2018-02-23 16:35:24 -0600

    Set up twig template using bootstrap

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pateldarshan
+ * Date: 2/22/18
+ * Time: 7:33 PM
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/",name="app_homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('homepage.html.twig');
+    }
+
+
+}
