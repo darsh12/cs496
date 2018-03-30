2e94aabaef0e818ec11b06b77a965ac6b7abdb39
Devin Lee <devintlee96@gmail.com>
2018-03-29 20:36:29 -0500

    created skeleton twig for homepage and individual tabs

---
+++ src/Controller/DynamicController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pateldarshan
+ * Date: 2/22/18
+ * Time: 7:33 PM
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DynamicController extends AbstractController
+{
+
+
+    /**
+     * @Route("/inventory",name="app_inventory")
+     */
+    public function inventory()
+    {
+        return $this->render('tabs/inventory.html.twig');
+    }
+
+    /**
+     * @Route("/battle",name="app_battle")
+     */
+    public function battle()
+    {
+        return $this->render('tabs/battle.html.twig');
+    }
+
+    /**
+     * @Route("/market",name="app_market")
+     */
+    public function market()
+    {
+        return $this->render('tabs/market.html.twig');
+    }
+
+
+}
